d75d05e741b78b7beef4760164a6bc4718a4ea45
smithchristopher23 <christopher67872@gmail.com>
2025-03-19 12:07:15 +0100

    telepites

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mysql = require("mysql")
+const app = express();
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const { error } = require("console");
+app.use(cors());
+app.use(bodyparser.json());
+
+const db = mysql.createConnection(
+    {
+        user: "root",
+        host: "localhost",
+        port: 3307,
+        password: "",
+        database: "felveteli"
+    }
+
+)
+app.get("/", (req, res) => {
+    res.send("Szerver működik!")
+}
+)
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("A server a 3000 porton fut!")
+})
